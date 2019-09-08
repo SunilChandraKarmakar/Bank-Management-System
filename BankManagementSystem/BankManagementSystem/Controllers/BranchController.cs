cabb72acd6926bee5175b1bfdb45339a456c550d
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-09-08 12:58:09 +0600

    Create Branch controller

---
+++ BankManagementSystem/BankManagementSystem/Controllers/BranchController.cs
@@ -0,0 +1,44 @@
+﻿using BankManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankManagementSystem.Controllers
+{
+    public class BranchController : Controller
+    {
+        private Bank_Management_System_Entities bms = new Bank_Management_System_Entities();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(bms.BRANCHes.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BRANCH aBranch, HttpPostedFileBase BRANCH_MANAGER_PICTURE)
+        {
+            aBranch.BRANCH_MANAGER_PICTURE = System.IO.Path.GetFileName(BRANCH_MANAGER_PICTURE.FileName);
+            aBranch.BRANCH_MANAGER_JOINDATE = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                bms.BRANCHes.Add(aBranch);
+                bms.SaveChanges();
+                BRANCH_MANAGER_PICTURE.SaveAs(Server.MapPath("../Uploads/Manager_Picture/" + aBranch.BRANCH_ID.ToString() + "_" + aBranch.BRANCH_MANAGER_PICTURE));
+                return RedirectToAction("Index");
+            }
+
+            return View(aBranch);
+        }
+    }
+}
